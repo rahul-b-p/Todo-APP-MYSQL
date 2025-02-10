417dcc6919fa450c6ff674faf586f2c5f29f7b1a
rahul-b-p <rahul@gmail.com>
2025-02-10 12:24:49 +0530

    feat:Add User Model

---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,15 @@
+import { Optional } from 'sequelize'
+import { Roles } from '../enums';
+
+
+export interface IUser {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    role: Roles;
+    refreshToken?: string;
+    verified: boolean
+}
+
+export interface IUserCreation extends Optional<IUser, "id"> { }; 
