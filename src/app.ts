109d76846cbc8e58d5d3eff9bd941e4bf9851cd1
Rahul B P <rahulzantoz10@gmail.com>
2025-02-10 11:40:28 +0530

    chore: initialize application with env and logger configurations

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import express from 'express';
+import './config/env.config';
+import { morganLogger } from './config';
+import { logger } from './utils';
+
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+/**
+ * Initializing the express application, and listening at the given port
+ */
+const InitializeApp = async () => {
+    try {
+
+
+        //using morgan logger on application
+        app.use(morganLogger);
+
+
+
+        // listening the application on port
+        app.listen(port, () => {
+            logger.info(`Server running successfully at http://localhost:${port}`);
+        });
+
+    } catch (error: any) {
+        logger.error('Failed to initialize application:', error);
+        process.exit(1);
+    }
+}
+
+InitializeApp();
+
+
+
+
+// Handle uncaught errors
+
+process.on('unhandledRejection', (error) => {
+    logger.error('Unhandled Rejection:', error);
+    process.exit(1);
+});
+
+process.on('uncaughtException', (error) => {
+    logger.error('Uncaught Exception:', error);
+    process.exit(1);
+});
