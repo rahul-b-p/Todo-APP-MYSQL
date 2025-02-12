604a60286cbc6ea3c49363be2ba6b7da77ef9006
rahul-b-p <rahulzantoz10@gmail.com>
2025-02-12 12:06:20 +0530

    feat(docker): Add initial Dockerfile and Docker Compose setup

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USERNAME } from "./env.config";
+import { MYSQL_HOST, MYSQL_NAME, MYSQL_PASSWORD, MYSQL_USERNAME } from "./env.config";
 
 
-export const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-    host: DB_HOST, // Replace with your server's hostname or IP
+export const sequelize = new Sequelize(MYSQL_NAME, MYSQL_USERNAME, MYSQL_PASSWORD, {
+    host: MYSQL_HOST, // Replace with your server's hostname or IP
     dialect: 'mysql', // Specifies the SQL dialect
     logging: false
 });
